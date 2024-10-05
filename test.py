3cd76fda133ea56c3e4fe678dfad4987eacd4758
Waji/ <zyyduwa@gmail.com>
2024-10-05 20:19:35 +0700

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ import time
 import os
 
 proxy_api_urls = [
-    #"https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt"
-    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all",
-    "https://www.proxy-list.download/api/v1/get?type=socks5",
-    "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/socks5.txt",
-    "https://sunny9577.github.io/proxy-scraper/generated/socks5_proxies.txt",
-    "https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/socks5.txt"                                                              ]
+    "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt"
+    #"https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all",
+    # "https://www.proxy-list.download/api/v1/get?type=socks5",
+    #"https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/socks5.txt",
+    #"https://sunny9577.github.io/proxy-scraper/generated/socks5_proxies.txt",
+    #"https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/socks5.txt"                                                              ]
 
 proxies = set()
 for url in proxy_api_urls:
